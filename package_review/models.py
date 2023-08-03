c993f6337c815c1da83aba7a47e5e213a1b02304
Hillel Arnold <hillel.arnold@gmail.com>
2023-08-03 19:07:52 -0400

    add Package and RightsStatement models

--- package_review/models.py
+++ package_review/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    """Package of digitized AV files."""
+    AUDIO = 1
+    VIDEO = 2
+    TYPE_CHOICES = (
+        (AUDIO, 'Audio'),
+        (VIDEO, 'Video'))
+
+    PENDING = 0
+    APPROVED = 9
+    REJECTED = 5
+    PROCESS_STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected'))
+
+    title = models.CharField(max_length=255)
+    av_number = models.CharField(max_length=255)
+    refid = models.CharField(max_length=32)
+    type = models.IntegerField(choices=TYPE_CHOICES)
+    process_status = models.IntegerField(choices=PROCESS_STATUS_CHOICES)
+    rights_ids = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.av_number} {self.title}'
+
+
+class RightsStatement(models.Model):
+    """Rights statement stored in Aquila."""
+
+    title = models.CharField(max_length=255)
+    aquila_id = models.IntegerField()
+    last_modified = models.DateTimeField(auto_now=True)
